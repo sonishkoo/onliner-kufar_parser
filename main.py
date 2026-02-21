a72e44f8d9df9d138251973403e1cef3aff5f723
sonishkoo <138611254+sonishkoo@users.noreply.github.com>
2026-02-21 16:20:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,6 @@ from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
 print("START parser_service.py")
 
-PROXIES = {
-    'http': 'http://proxy.todes.by:9999',
-    'https': 'http://proxy.todes.by:9999',
-}
-
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0"}
 onliner_url = 'https://baraholka.onliner.by/'
 kufar_api = 'https://api.kufar.by/search-api/v2/search/rendered-paginated'
@@ -332,3 +327,4 @@ if __name__ == "__main__":
         exit()
 
     # http://127.0.0.1:8000/cost_parser/api/v1/search?q=%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA+lenovo&excluded=%D0%B7%D0%B0%D0%BF%D1%87%D0%B0%D1%81%D1%82&min_price=700&max_price=1000
+
